f15075c3056e5882a5e171ccf3e7f2fd307eae09
Erisvaldo <erisvaldodesenvolvedor@gmail.com>
2024-06-04 16:04:50 -0300

    Containers rodando, vou agora colocar o projeto inicial no aspnetcore

--- ERP/backend/backend_aspnetcore/Program.cs
+++ ERP/backend/backend_aspnetcore/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Hello backend_aspnetcore!");
 
 app.Run();
